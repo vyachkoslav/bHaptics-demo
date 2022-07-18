18b1e0a6c78bb69e593b8231fb46cb120ddeaf5d
Vyachkoslav <dedplantoed@gmail.com>
2022-07-18 13:35:57 +0300

    Added menu scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
     public static void WinGame()
     {
         SceneManager.LoadScene("Demo");
