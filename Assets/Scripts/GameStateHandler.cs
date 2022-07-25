f0a0a82559f7a10be38418eeb680c00019d6eb76
Vyachkoslav <dedplantoed@gmail.com>
2022-07-25 12:43:40 +0300

    Added reset player position button to demo

--- Assets/Scripts/GameStateHandler.cs
+++ Assets/Scripts/GameStateHandler.cs
@@ -25,29 +25,60 @@ public class GameStateHandler : MonoBehaviour
     /// </summary>
     [SerializeField] List<GameObject> objectsToHide;
 
+    // Player reset
+    [SerializeField] Transform playerOrigin;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] Vector3 defaultPosition;
+
+    [SerializeField] PlayerData playerData;
+
     void Start()
     {
         endMenu.SetActive(false);
-        Time.timeScale = 1f;
+        SetTimeScale(1);
+        
+
+        if(playerData)
+            playerOrigin.position = playerData.originPosition;
     }
 
-    public void HandleLoose()
+    public void HandleLoose(string reason)
     {
-        if(creator)
+        SetTimeScale(0.5f);
+        StartCoroutine(ShowMenuAfterTime(reason, 2));
+    }
+    IEnumerator ShowMenuAfterTime(string title, float seconds)
+    {
+        yield return new WaitForSecondsRealtime(seconds);
+        if (creator)
             creator.DestroyAllCreatedObjects();
         endMenu.SetActive(true);
-        menuTitle.text = "Game Over";
+        menuTitle.text = title;
 
         scriptsToActivate.ForEach(x => x.enabled = true);
         objectsToHide.ForEach(x => x.SetActive(false));
         var dots = FindObjectsOfType<BhapticsDotPoint>();
-        foreach(var dot in dots)
+        foreach (var dot in dots)
             dot.enabled = false;
-        Time.timeScale = 0f;
+
+        SetTimeScale(0);
     }
     public void HandleWin()
     {
-        HandleLoose();
-        menuTitle.text = "You won!";
+        HandleLoose("You won!");
+    }
+    public void PlayerReset()
+    {
+        playerOrigin.position += defaultPosition - playerTransform.position;
+
+        if(playerData)
+            playerData.originPosition = playerOrigin.position;
+    }
+
+    static void SetTimeScale(float scale)
+    {
+        Time.timeScale = scale;
+        List<AudioSource> audios = new(FindObjectsOfType<AudioSource>());
+        audios.ForEach(x => x.pitch = scale);
     }
 }
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Saves player data between scenes
+/// </summary>
+[CreateAssetMenu(fileName = "Player data", menuName = "ScriptableObjects/Player Data", order = 1)]
+public class PlayerData : ScriptableObject
+{
+    public Vector3 originPosition;
+}
